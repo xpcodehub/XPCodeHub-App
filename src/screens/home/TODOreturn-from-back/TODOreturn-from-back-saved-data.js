379dff59767356365a9fed3183d219481c2cf669
GustavoSTZ <stzgustavo@gmail.com>
2022-02-28 20:09:38 -0300

    Tela home de forma estÃatica

---
+++ src/screens/home/TODOreturn-from-back/TODOreturn-from-back-saved-data.js
@@ -0,0 +1,20 @@
+export const data = {
+        'programming-logic':
+            {
+                optional: false,
+                nodeState: 'selected'
+            },
+    'gradle':
+        {
+            optional: false,
+            nodeState: 'unlocked'
+        },
+    'oop':
+        {
+            optional: true,
+            nodeState: 'locked'
+        }
+    }
+;
+
+export default data;
--- src/screens/home/sections/skill-tree.section.jsx
+++ src/screens/home/sections/skill-tree.section.jsx
@@ -6,18 +6,46 @@ import {
     SkillProvider
 } from 'beautiful-skill-tree';
 import theme from '../sections/skill-tree.theme.js';
+import data from '../TODOreturn-from-back/TODOreturn-from-back-saved-data'; //TODO Tirar
+import {Redirect} from "react-router-dom";
 
 
 export class SkillTreeSection extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            redireciona: false,
+        }
+    }
+
+    getSkillTreeSavedData = (treeId) => {
+        return data;
+    };
+
+    selectSkill = async (skillId) => {
+        await new Promise(r => setTimeout(r, 400)); // Timer para dar tempo do efeito do click. Faz sentido ter isso? //TODO
+        this.setState({
+            redireciona: `/tech-steps/${skillId}`
+        })
+    };
+
     render() {
+        if (this.state.redireciona) {
+            return <Redirect to={this.state.redireciona}/>
+        }
             return (
                 <>
                     <SkillProvider>
                         <SkillTreeGroup theme={theme}>
                             {() => {
                                 return (
-                                    <SkillTree treeId={this.props.data[0].tree.title} title={this.props.data[0].tree.title} data={this.props.data} />
+                                    <SkillTree treeId={this.props.data.tree.title}
+                                               title={this.props.data.tree.title}
+                                               data={this.props.data.tree.root}
+                                               savedData={this.getSkillTreeSavedData(this.props.data.tree.id)}
+                                               handleNodeSelect={(event) => this.selectSkill(event.key)}
+                                    />
                                 )
                             }}
                         </SkillTreeGroup>
